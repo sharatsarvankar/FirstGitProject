23ba8a7159c5ed897da561f2a9ab5ca5410176ba
SarvankarS <SarvankarS@E0408679.internal.vodafone.com>
2019-02-24 15:40:09 +0530

    new msg

--- FirstGitProject/src/test/HelloGit.java
+++ FirstGitProject/src/test/HelloGit.java
@@ -5,6 +5,8 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.print("hellow Git"); 
+    System.out.print("hellow Git"); 
+    
 	}
 
 }
