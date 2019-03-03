c7b8ef74df47646204ae6335a88a038cf0914ed8
SarvankarS <sharat.sarvankar@gmail.com>
2019-03-03 15:35:52 +0530

    1st commit

--- FirstGitProject/src/test/HelloGit.java
+++ FirstGitProject/src/test/HelloGit.java
@@ -5,6 +5,12 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.print("hellow Git"); 
+    System.out.print("hellow Git -1 "); 
+    for (int i=0; i<10;i++)
+    {
+    	 System.out.println("hellow Git -1 "+i); 
+    	
+    }
 	}
 
 }
